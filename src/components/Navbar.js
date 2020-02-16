06675780d90138882b175ee4c473c4da2d928a52
Hunter Redding <hunterred50@gmail.com>
2020-02-15 20:25:33 -0500

    fixed spacing [format]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,139 +23,139 @@ const divForText = {display: "block",
                     objectFit: "cover" }
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayBio: true,
-            displaySite: true,
-            displayMusic: false,
-            displayPhoto: false,
-            displayVid: false,
-            displayArt: false,
-            displayCode: false,
-            displayContact: true
-        }
-        this.clearState = this.clearState.bind(this);
-        this.toggleBio = this.toggleBio.bind(this);
-        this.toggleSite = this.toggleSite.bind(this);
-        this.toggleMusic = this.toggleMusic.bind(this);
-        this.togglePhoto = this.togglePhoto.bind(this);
-        this.toggleVid = this.toggleVid.bind(this);
-        this.toggleArt = this.toggleArt.bind(this);
-        this.toggleCode = this.toggleCode.bind(this);
-        this.toggleContact = this.toggleContact.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayBio: true,
+      displaySite: true,
+      displayMusic: false,
+      displayPhoto: false,
+      displayVid: false,
+      displayArt: false,
+      displayCode: false,
+      displayContact: true
     }
-    clearState() {
-        this.setState(state => ({
-            displayBio: false,
-            displaySite: false,
-            displayMusic: false,
-            displayPhoto: false,
-            displayVid: false,
-            displayArt: false,
-            displayCode: false,
-            displayContact: false
-        }));
-    }
-    toggleBio() {
-        this.setState(state => ({
-            displayBio: !state.displayBio
-        }));
-    }
-    toggleSite() {
-        this.setState(state => ({
-            displaySite: !state.displaySite
-        }));
-    }
-    toggleMusic() {
-        this.setState(state => ({
-            displayMusic: !state.displayMusic
-        }));
-    }
-    togglePhoto() {
-        this.setState(state => ({
-            displayPhoto: !state.displayPhoto
-        }));
-    }
-    toggleVid() {
-        this.setState(state => ({
-            displayVid: !state.displayVid
-        }));
-    }
-    toggleArt() {
-        this.setState(state => ({
-            displayArt: !state.displayArt
-        }));
-    }
-    toggleCode() {
-        this.setState(state => ({
-            displayCode: !state.displayCode
-        }));
-    }
-    toggleContact = () => {
-        this.setState(state => ({
-            displayContact: !state.displayContact
-        }));
-    }
-    render() {
-        return (
-            <div className="nav">
-                <div className="nav-item" onClick={this.clearState}> {/*option could be to reset to initial state, which would be startup*/}
-                    <Icon name="computer_5"/>
-                    <p>home</p>
-                </div>
+    this.clearState = this.clearState.bind(this);
+    this.toggleBio = this.toggleBio.bind(this);
+    this.toggleSite = this.toggleSite.bind(this);
+    this.toggleMusic = this.toggleMusic.bind(this);
+    this.togglePhoto = this.togglePhoto.bind(this);
+    this.toggleVid = this.toggleVid.bind(this);
+    this.toggleArt = this.toggleArt.bind(this);
+    this.toggleCode = this.toggleCode.bind(this);
+    this.toggleContact = this.toggleContact.bind(this);
+  }
+  clearState() {
+    this.setState(state => ({
+      displayBio: false,
+      displaySite: false,
+      displayMusic: false,
+      displayPhoto: false,
+      displayVid: false,
+      displayArt: false,
+      displayCode: false,
+      displayContact: false
+    }));
+  }
+  toggleBio() {
+    this.setState(state => ({
+      displayBio: !state.displayBio
+    }));
+  }
+  toggleSite() {
+    this.setState(state => ({
+      displaySite: !state.displaySite
+    }));
+  }
+  toggleMusic() {
+    this.setState(state => ({
+      displayMusic: !state.displayMusic
+    }));
+  }
+  togglePhoto() {
+    this.setState(state => ({
+      displayPhoto: !state.displayPhoto
+    }));
+  }
+  toggleVid() {
+    this.setState(state => ({
+      displayVid: !state.displayVid
+    }));
+  }
+  toggleArt() {
+    this.setState(state => ({
+      displayArt: !state.displayArt
+    }));
+  }
+  toggleCode() {
+    this.setState(state => ({
+      displayCode: !state.displayCode
+    }));
+  }
+  toggleContact = () => {
+    this.setState(state => ({
+      displayContact: !state.displayContact
+    }));
+  }
+  render() {
+    return (
+      <div className="nav">
+        <div className="nav-item" onClick={this.clearState}> {/*option could be to reset to initial state, which would be startup*/}
+          <Icon name="computer_5"/>
+          <p>home</p>
+        </div>
 
-                <div className="nav-item" onClick={this.toggleBio}>
-                    <Icon name="notepad"/>
-                    <p>about me</p>
-                </div>
-                {this.state.displayBio && <Bio display={this.toggleBio} />}
+        <div className="nav-item" onClick={this.toggleBio}>
+          <Icon name="notepad"/>
+          <p>about me</p>
+        </div>
+        {this.state.displayBio && <Bio display={this.toggleBio} />}
 
-                {this.state.displaySite && <Modal title="About this site" closeModal={this.toggleSite} icon="defrag" className="modal" 
-                style={{left: "27%", top: "40%", height: 170, width: 310}}>
-                    This site was developed using React!<br /><br />Click and drag the blue bar at the top of this window around!<br/><br/>Wow! This was implemented using the UI library React95.
-                </Modal>}
+        {this.state.displaySite && <Modal title="About this site" closeModal={this.toggleSite} icon="defrag" className="modal" 
+          style={{left: "27%", top: "40%", height: 170, width: 310}}>
+            This site was developed using React!<br /><br />Click and drag the blue bar at the top of this window around!<br/><br/>Wow! This was implemented using the UI library React95.
+        </Modal>}
 
-                <div className="nav-item" onClick={this.toggleMusic}>
-                    <Icon name="media_cd"/>
-                    <p>music</p> {/* click to play a random song from a playlist of songs I've been a part of */}
-                </div>
-                {this.state.displayMusic && <Music display={this.toggleMusic} />}
+        <div className="nav-item" onClick={this.toggleMusic}>
+          <Icon name="media_cd"/>
+          <p>music</p> {/* click to play a random song from a playlist of songs I've been a part of */}
+        </div>
+        {this.state.displayMusic && <Music display={this.toggleMusic} />}
 
-                <div className="nav-item" onClick={this.togglePhoto}>
-                    <Icon name="camera"/>
-                    <p>photography</p>
-                </div>
-                {this.state.displayPhoto && <Photo display={this.togglePhoto} />}
+        <div className="nav-item" onClick={this.togglePhoto}>
+          <Icon name="camera"/>
+          <p>photography</p>
+        </div>
+        {this.state.displayPhoto && <Photo display={this.togglePhoto} />}
 
-                <div className="nav-item" onClick={this.toggleVid}>
-                    <Icon name="media_video"/>
-                    <p>video</p>
-                </div>
-                {this.state.displayVid && <Vid display={this.toggleVid} />}
+        <div className="nav-item" onClick={this.toggleVid}>
+          <Icon name="media_video"/>
+          <p>video</p>
+        </div>
+        {this.state.displayVid && <Vid display={this.toggleVid} />}
 
-                <div className="nav-item" onClick={this.toggleArt}>
-                    <Icon name="mspaint"/>
-                    <p>visual art</p>
-                </div>
-                {this.state.displayArt && <Art display={this.toggleArt} />}
+        <div className="nav-item" onClick={this.toggleArt}>
+          <Icon name="mspaint"/>
+          <p>visual art</p>
+        </div>
+        {this.state.displayArt && <Art display={this.toggleArt} />}
 
-                <div className="nav-item" onClick={this.toggleCode}>
-                    <Icon name="defrag"/>
-                    <p>coding</p>
-                </div>
-                {this.state.displayCode && <Code display={this.toggleCode} />}
+        <div className="nav-item" onClick={this.toggleCode}>
+          <Icon name="defrag"/>
+          <p>coding</p>
+        </div>
+        {this.state.displayCode && <Code display={this.toggleCode} />}
 
-                <div className="nav-item" onClick={this.toggleContact}>
-                    <Icon name="phone_2"/>
-                    <p>contact me</p>
-                </div>
-                {this.state.displayContact && <Contact display={this.toggleContact} />}
+        <div className="nav-item" onClick={this.toggleContact}>
+          <Icon name="phone_2"/>
+          <p>contact me</p>
+        </div>
+        {this.state.displayContact && <Contact display={this.toggleContact} />}
 
-                <Button>click me!</Button>
-            </div>
-        )
-    }
+        <Button>click me!</Button>
+      </div>
+    )
+  }
 }
 export default Navbar
 
