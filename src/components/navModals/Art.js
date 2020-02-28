57ac26c6e0c7b6c6d1a7e1d2407acedd4b786f63
Hunter Redding <hunterred50@gmail.com>
2020-02-28 18:19:34 -0500

    removed IG widget

--- src/components/navModals/Art.js
+++ src/components/navModals/Art.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Modal from '@react95/core/Modal'
 
-class Art extends Component {
-  render() {
-    return (
-      <Modal title="My art" closeModal={this.props.display} icon="mspaint" className="modal" 
-        style={{right: "30%", top: "15%", height: "80%", width: "30%"}}>
-          <div style={{width: "100%", height: "100%", overflowY: "scroll"}}>
-            <h3>Sketchbook instagram feed</h3> <br />
-            {/* LightWidget WIDGET */} 
-            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-            <iframe title="insta" src="//lightwidget.com/widgets/d41457aac05c5cd5ade6cb3d956c2730.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" 
-            style={{width: "100%", height: "100%", border: "0"}}></iframe><br />
-          </div>
-      </Modal>
-    )
-  }
+function Art(props) {
+  return (
+    <Modal title="My art" closeModal={props.display} icon="mspaint" className="modal" 
+      style={{left: "10%", top: "5%", height: "90%", width: "80%",}}>
+        <div style={{width: "100%", height: "100%", overflowY: "scroll"}}>
+          {/* <h3>Sketchbook instagram feed</h3> <br />
+          {/* LightWidget WIDGET */} 
+          {/* <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+          <iframe title="insta" src="//lightwidget.com/widgets/d41457aac05c5cd5ade6cb3d956c2730.html" scrolling="no" allowtransparency="true" className="lightwidget-widget" 
+          style={{width: "70%", height: "100%", border: "0", display: "block", margin: "auto"}}></iframe><br /> */}
+        </div>
+    </Modal>
+  )
 }
 
 export default Art
