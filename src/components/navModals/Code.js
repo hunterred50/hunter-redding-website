0f18cf8a4fb6b26512ce918f6dd6d72dc90cee29
Hunter Redding <hunterred50@gmail.com>
2020-03-10 22:31:40 -0500

    fixed some style on code modal

--- src/components/navModals/Code.js
+++ src/components/navModals/Code.js
@@ -5,7 +5,7 @@ class Code extends Component {
   render() {
     return (
       <Modal title="Coding portfolio" closeModal={this.props.display} icon="defrag" className="modal" 
-        style={{right: "30%", top: "15%", height: 150}}>
+        style={{right: "30%", top: "20%", height: "10%", width: "60%"}}>
         <a href="https://gdurl.com/Jgwe" target="_blank" rel="noopener noreferrer">resume</a><br />
       </Modal>
     )
