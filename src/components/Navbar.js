8c88660b23c0362b1c421485121452949bb23a62
Hunter Redding <hunterred50@gmail.com>
2020-03-19 23:33:55 -0500

    added clearState & Bio, fixed mobile comp display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import Button from '@react95/core/Button'
 import Icon from '@react95/core/Icon'
 import List from '@react95/core/List'
 
+import Bio from './navModals/Bio'
 import Music from './navModals/Music'
 import Photo from './navModals/Photo'
 import Vid from './navModals/Vid'
@@ -15,16 +16,18 @@ import { Breakpoint } from 'react-socks'
 import { Link } from 'react-router-dom'
 
 function Navbar (props) {
+
   let history = useHistory();
   function handleClick(where) {
     history.push("/" + where);
   }
+
   return (
     <div>
       <Breakpoint medium up>
         <div className="nav">
           {/* <div className="nav-item" onClick={props.clearState}> option could be to reset to initial state, which would be startup */}
-            <Link className="nav-item" to="/">
+            <Link className="nav-item" to="/" onClick={props.clearState}>
               <Icon name="computer_5"/>
               <p>home</p>
             </Link>
@@ -34,6 +37,7 @@ function Navbar (props) {
             <Icon name="notepad"/>
             <p>about me</p>
           </div>
+          {props.displayBio && <Bio display={props.toggleBio} />}
 
           <div className="nav-item" onClick={props.toggleMusic}> {/* maybe close other sectional modals */}
             <Icon name="media_cd"/>
@@ -69,9 +73,9 @@ function Navbar (props) {
             <Icon name="phone_2"/>
             <p>contact me</p>
           </div>
-          {props.displayContact && <Contact display={props.toggleContact} />}
+          {!props.displayContact && <Contact display={props.toggleContact} />}
 
-          <Button onClick={props.changeBkgd}>click me!</Button>
+          {/* <Button onClick={props.changeBkgd}>click me!</Button> */}
         </div>
       </Breakpoint>
       
@@ -79,7 +83,7 @@ function Navbar (props) {
         <List className="dropdown" style={{width: "40%"}}>
           <List.Item icon="logo" title="START">
             START
-            <List className="dropdown">
+            <List className="dropdown" onClick={props.clearState}>
               <List.Item className="drop-item" onClick={() => handleClick("")}>
                 <Icon name="computer_5"/>
                 <p>home</p>
@@ -94,39 +98,33 @@ function Navbar (props) {
                 <Icon name="media_cd"/>
                 <p>music</p> {/* click to play a random song from a playlist of songs I've been a part of */}
               </List.Item>
-              {props.displayMusic && <Music display={props.toggleMusic} />}
 
               <List.Item className="drop-item" onClick={() => handleClick("photo")}>
                 <Icon name="camera"/>
                 <p>photography</p>
               </List.Item>
-              {props.displayPhoto && <Photo display={props.togglePhoto} />}
 
               <List.Item className="drop-item" onClick={() => handleClick("vid")}>
                 <Icon name="media_video"/>
                 <p>video</p>
               </List.Item>
-              {props.displayVid && <Vid display={props.toggleVid} />}
 
               <List.Item className="drop-item" onClick={() => handleClick("art")}>
                 <Icon name="mspaint"/>
                 <p>visual art</p>
               </List.Item>
-              {props.displayArt && <Art display={props.toggleArt} />}
 
               <List.Item className="drop-item" onClick={() => handleClick("code")}>
                 <Icon name="defrag"/>
                 <p>coding</p>
               </List.Item>
-              {props.displayCode && <Code display={props.toggleCode} />}
 
               <List.Item className="drop-item" onClick={() => handleClick("contact")}>
                 <Icon name="phone_2"/>
                 <p>contact me</p>
               </List.Item>
-              {!props.displayContact && <Contact display={props.toggleContact} />}
 
-              <Button onClick={props.changeBkgd}>click me!</Button>
+              {/* <Button onClick={props.changeBkgd}>click me!</Button> */}
             </List>
           </List.Item>
         </List>
