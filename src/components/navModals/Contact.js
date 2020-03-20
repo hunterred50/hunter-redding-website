5600ca2ccdc92922ab3cfb4b60162dcc43cbd83c
Hunter Redding <hunterred50@gmail.com>
2020-03-19 23:35:19 -0500

    fixed closing function

--- src/components/navModals/Contact.js
+++ src/components/navModals/Contact.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 import Modal from '@react95/core/Modal'
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
-        <Modal title="Contact me" closeModal={this.props.display} icon="phone_2" className="modal" 
+        {!this.props.close && <Modal title="Contact me" closeModal={this.props.display} icon="phone_2" className="modal" 
         style={{right: "2%", top: "8%", height: 150, width: 200}}>
           Hunter Redding<br />
           hunterred50@gmail.com<br /><br />
           <a href="https://www.linkedin.com/in/hunter-redding/" target="_blank" rel="noopener noreferrer">linkedin</a>
           <a href="https://github.com/hunterred50" target="_blank" rel="noopener noreferrer">github</a>
-        </Modal>
+        </Modal>}
       </div>
     )
   }
