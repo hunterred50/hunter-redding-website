c8ebefb54c1b6a5c2a7d34bfb3ed53e1d95238a2
Hunter Redding <hunterred50@gmail.com>
2020-09-18 20:12:11 -0400

    updated bio

--- src/components/navModals/Bio.js
+++ src/components/navModals/Bio.js
@@ -28,13 +28,13 @@ class Bio extends Component {
           <img src="https://gdurl.com/pTtP" alt="me" style={imgStyle}/>
           <div style={divForText}>
             <Breakpoint medium up>
-              <p><br />Hello, welcome to my website!</p><br />My name is Hunter Redding, I'm a software developer, musician, and an artist of many forms.<br /><br />
-              I'm currently a freelance web developer and artist, if you have interest in working with me, check out the contact window above, thanks!<br /><br />
+              <p><br />Hello, welcome to my website!</p><br />My name is Hunter Redding, I'm a videographer, web developer, musician, and artist.<br /><br />
+              I'm currently employed full time as a webmaster/video specialist, but if you have interest in working with me as an artist, check out the contact window above, thanks!<br /><br />
               Feel free to use the icons at the top to explore my various works :)
             </Breakpoint>
             <Breakpoint small down>
-              <p><br />Hello, welcome to my website!</p><br />My name is Hunter Redding, I'm a software developer, musician, and an artist of many forms.<br /><br />
-              I'm currently a freelance web developer and artist, if you have interest in working with me, check out the contact window above, thanks!<br /><br />
+              <p><br />Hello, welcome to my website!</p><br />My name is Hunter Redding, I'm a videographer, web developer, musician, and artist.<br /><br />
+              I'm currently employed full time as a webmaster/video specialist, but if you have interest in working with me as an artist, check out the contact window above, thanks!<br /><br />
               Feel free to use the start button at the top to explore my various works :)
             </Breakpoint>
           </div>
